c0b9af575ebf1bf48540182600790ce8ddf3f291
Richard Remer <reanjr@gmail.com>
2016-10-28 19:55:54 -0700

    better handling of alt key

--- src/rico.c
+++ src/rico.c
@@ -16,6 +16,7 @@ Rico rico_create() {
 bool rico_run(Rico rico) {
     int key;
     char* name;
+    bool alt;
 
     if (rico->running) return false;
 
@@ -29,6 +30,11 @@ bool rico_run(Rico rico) {
     rico->running = true;
 
     while (false != (key = rico_scan(rico))) {
+        alt = key & (1<<16);
+        key &= ~(1<<16);
+
+        if (alt) rico_out(rico, "⌥ +");
+
         rico_outi(rico, key);
         name = malloc(snprintf(NULL, 0, ":%s ", keyname(key)));
         sprintf(name, ":%s ", keyname(key));
@@ -94,7 +100,7 @@ int rico_scan(Rico rico) {
             timeout(-1);
 
             if (key == ERR) key = 27;
-            else key += 1000;
+            else key += (1 << 16);
         }
     }
 
